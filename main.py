44fcf155dcbc1e83d7d67c500480f0b545ad1101
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-11 21:25:18 -0400

    Update main.py
    Improve cogs loading with threads

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from nextcord.ext import commands
 from nextcord import Intents
+from threading import Thread
 bot = commands.Bot(command_prefix=['?'], help_command=None, intents=Intents.all())
-from os import listdir, environ
+from os import listdir, environ, system
 for i in listdir('cogs'):
   if i.endswith('.py'):
-    bot.load_extension(f'cogs.{i[: -3]}')
+    def hi():
+      bot.load_extension(f'cogs.{i[: -3]}')
+    t = Thread(target=hi)
+    t.start()
 bot.run(environ['token'])
